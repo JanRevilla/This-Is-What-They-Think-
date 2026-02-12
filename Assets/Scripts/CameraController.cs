e801b34a9507a954bb0ef227d56a81455774099f
ElAndamio <adamguardiacar@gmail.com>
2026-02-12 17:23:33 +0100

    Movimiento del Personaje + Movimiento realista de la cámara

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,89 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Header("Referencias")]
+    public Transform m_PlayerBody; 
+    public Transform m_PitchController;
+    private CharacterController m_CharacterController;
+    public PlayerController m_PlayerController;
+
+    [Header("Rotación (Mouse Look)")]
+    public float m_YawSpeed = 150f;
+    public float m_PitchSpeed = 150f;
+    public float m_MinPitch = -80f;
+    public float m_MaxPitch = 80f;
+    public bool m_UseInvertedYaw = false;
+    public bool m_UseInvertedPitch = false;
+    public KeyCode m_AngleLockedKeycode = KeyCode.I;
+
+    [Header("Head Bobbing (Rebote)")]
+    public bool m_UseBobbing = true;
+    [Range(0, 0.1f)] public float m_BobAmplitude = 0.015f;
+    [Range(0, 30)] public float m_BobFrequency = 12.0f;
+
+    private float m_Yaw;
+    private float m_Pitch;
+    private bool m_AngleLocked = false;
+    private Vector3 m_CameraStartPos;
+    private float m_BobTimer;
+
+    private void Start()
+    {
+        m_CharacterController = m_PlayerController.GetComponent<CharacterController>();
+
+        m_CameraStartPos = transform.localPosition;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        m_Yaw = m_PlayerBody.eulerAngles.y;
+        float initialPitch = m_PitchController.localEulerAngles.x;
+        if (initialPitch > 180) initialPitch -= 360;
+        m_Pitch = initialPitch;
+    }
+
+    private void Update()
+    {
+        HandleRotation();
+        if (m_UseBobbing) HandleHeadBob();
+    }
+
+    private void HandleRotation()
+    {
+        if (Input.GetKeyDown(m_AngleLockedKeycode))
+            m_AngleLocked = !m_AngleLocked;
+
+        if (m_AngleLocked) return;
+
+        float l_MouseX = Input.GetAxis("Mouse X");
+        float l_MouseY = Input.GetAxis("Mouse Y");
+
+        m_Yaw += l_MouseX * m_YawSpeed * Time.deltaTime * (m_UseInvertedYaw ? -1.0f : 1.0f);
+        m_Pitch += l_MouseY * m_PitchSpeed * Time.deltaTime * (m_UseInvertedPitch ? -1.0f : 1.0f);
+        m_Pitch = Mathf.Clamp(m_Pitch, m_MinPitch, m_MaxPitch);
+
+        m_PlayerBody.rotation = Quaternion.Euler(0.0f, m_Yaw, 0.0f);
+        m_PitchController.localRotation = Quaternion.Euler(m_Pitch, 0.0f, 0.0f);
+    }
+
+    private void HandleHeadBob()
+    {
+        float l_Speed = new Vector3(m_CharacterController.velocity.x, 0, m_CharacterController.velocity.z).magnitude;
+
+        if (l_Speed > 0.5f && m_CharacterController.isGrounded)
+        {
+            m_BobTimer += Time.deltaTime * (m_BobFrequency * (l_Speed/m_PlayerController.m_MaxSpeed));
+            Debug.Log(m_BobTimer);
+
+            float posX = m_CameraStartPos.x + Mathf.Sin(m_BobTimer * 0.5f) * m_BobAmplitude;
+            float posY = m_CameraStartPos.y + Mathf.Sin(m_BobTimer) * m_BobAmplitude;
+
+            transform.localPosition = new Vector3(posX, posY, m_CameraStartPos.z);
+        }
+        else
+        {
+            //m_BobTimer = 0;
+            transform.localPosition = Vector3.Lerp(transform.localPosition, m_CameraStartPos, Time.deltaTime * 10f);
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,151 +1,73 @@
-using System;
-using System.Collections;
-using TMPro;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header ("Rotation")]
-
-    private float m_Yaw;
-    private float m_Pitch;
-
-    public float m_YawSpeed;
-    public float m_PitchSpeed;
-
-    public float m_MinPitch;
-    public float m_MaxPitch;
-
-    public Transform m_PitchController;
-
-    public bool m_UseInvertedYaw;
-    public bool m_UseInvertedPitch;
-
-    [Header("Movement")]
-
+    [Header("Movement Settings")]
     public CharacterController m_CharacterController;
-
-    private float m_VerticalSpeed;
-
-    public float m_Speed;
-    public float m_SpeedMultiplier;
-    public float m_JumpSpeed;
-
-    public float m_GravityMultiplier;
-
-    private bool m_AngleLocked = false;
-
-    [Header ("Camera")]
-
-    public Camera m_Camera;
+    public float m_MaxSpeed = 5.0f;
+    public float m_RunMultiplier = 2.0f;
+    public float m_Acceleration = 10.0f;
+    public float m_Deceleration = 12.0f;
+    public float m_JumpSpeed = 8.0f;
+    public float m_GravityMultiplier = 2.0f;
 
     [Header("Input")]
-
-    public KeyCode m_LeftKeyCode = KeyCode.A;
-    public KeyCode m_RightKeyCode = KeyCode.D;
-    public KeyCode m_UpKeyCode = KeyCode.W;
-    public KeyCode m_DownKeyCode = KeyCode.S;
-    public KeyCode m_JumpKeyCode = KeyCode.Space;
     public KeyCode m_RunKeyCode = KeyCode.LeftShift;
-    public KeyCode m_ReloadKeyCode = KeyCode.R;
-    public int m_ShootMouseButton = 0;
 
-    [Header("Debug Input")]
-    public KeyCode m_AngleLockedKeycode = KeyCode.I;
+    private Vector3 m_CurrentHorizontalVelocity;
+    private float m_VerticalSpeed;
 
-
-    private void Start()
+    private void Awake()
     {
-        /* PlayerController l_Player = GameManager.GetGameManager().GetPlayer();     // evitar que es crein + players entre scenes
-        if (l_Player != null)
-        {
-            l_Player.m_CharacterController.enabled = false;
-            l_Player.transform.position = transform.position;
-            l_Player.transform.rotation = transform.rotation;
-            l_Player.m_CharacterController.enabled = true;
-
-            l_Player.m_RespawnPosition = transform.position;
-            l_Player.m_StartRotation = transform.rotation;
-
-            GameObject.Destroy(gameObject);
-            return;
-        } */
-
-        // m_RespawnPosition = transform.position;
-        // m_StartRotation = transform.rotation;
-
-        DontDestroyOnLoad(gameObject); 
-
-        // GameManager.GetGameManager().SetPlayer(this);
-
-        Cursor.lockState = CursorLockMode.Locked;
+        m_CharacterController = GetComponent<CharacterController>();
+        DontDestroyOnLoad(gameObject);
     }
 
     private void Update()
     {
-        float l_MouseX = Input.GetAxis("Mouse X");
-        float l_MouseY = Input.GetAxis("Mouse Y");
+        HandleMovement();
+    }
 
-        if (Input.GetKeyDown(m_AngleLockedKeycode)) // bloqueig de camera
-            m_AngleLocked = !m_AngleLocked;
+    private void HandleMovement()
+    {
+        float inputX = 0;
+        float inputZ = 0;
 
-        if (!m_AngleLocked)
+        if (Input.GetKey(KeyCode.D)) inputX = 1;
+        else if (Input.GetKey(KeyCode.A)) inputX = -1;
+
+        if (Input.GetKey(KeyCode.W)) inputZ = 1;
+        else if (Input.GetKey(KeyCode.S)) inputZ = -1;
+
+        Vector3 targetDirection = (transform.forward * inputZ + transform.right * inputX).normalized;
+
+        float currentMaxSpeed = m_MaxSpeed * (Input.GetKey(m_RunKeyCode) ? m_RunMultiplier : 1.0f);
+        Vector3 targetVelocity = targetDirection * currentMaxSpeed;
+
+        float lerpSpeed = (targetDirection.magnitude > 0) ? m_Acceleration : m_Deceleration;
+
+        m_CurrentHorizontalVelocity = Vector3.Lerp(
+            m_CurrentHorizontalVelocity,
+            targetVelocity,
+            lerpSpeed * Time.deltaTime
+        );
+
+        m_VerticalSpeed += Physics.gravity.y * m_GravityMultiplier * Time.deltaTime;
+
+        Vector3 finalMovement = m_CurrentHorizontalVelocity;
+        finalMovement.y = m_VerticalSpeed;
+
+        CollisionFlags l_Flags = m_CharacterController.Move(finalMovement * Time.deltaTime);
+
+        if ((l_Flags & CollisionFlags.Below) != 0)
         {
-            m_Yaw = m_Yaw + l_MouseX * m_YawSpeed * Time.deltaTime * (m_UseInvertedYaw ? -1.0f : 1.0f);
-            m_Pitch = m_Pitch + l_MouseY * m_PitchSpeed * Time.deltaTime * (m_UseInvertedPitch ? -1.0f : 1.0f);
-
-            m_Pitch = Mathf.Clamp(m_Pitch, m_MinPitch, m_MaxPitch);
-
-            transform.rotation = Quaternion.Euler(0.0f, m_Yaw, 0.0f);
-            m_PitchController.localRotation = Quaternion.Euler(m_Pitch, 0.0f, 0.0f);
-
+            m_VerticalSpeed = 0;
+            //Salto
+            //if (Input.GetKeyDown(KeyCode.Space)) m_VerticalSpeed = m_JumpSpeed;
         }
-
-        Vector3 l_Movement = Vector3.zero;
-
-        //float l_YawPiRadians = m_Yaw * Mathf.Deg2Rad;
-        //float l_Yaw90PiRadians = (m_Yaw + 90) * Mathf.Deg2Rad;
-
-        //Vector3 l_ForwardDirection = new Vector3(Mathf.Sin(l_YawPiRadians), 0.0f, Mathf.Cos(l_YawPiRadians));
-        //Vector3 l_RightDirection = new Vector3(Mathf.Sin(l_Yaw90PiRadians), 0.0f, Mathf.Cos(l_Yaw90PiRadians));
-
-        Vector3 l_ForwardDirection = transform.forward; 
-        Vector3 l_RightDirection = transform.right;
-
-        if (Input.GetKey(m_RightKeyCode))
-            l_Movement = l_RightDirection;
-        else if (Input.GetKey(m_LeftKeyCode))
-            l_Movement += -l_RightDirection;
-
-        if (Input.GetKey(m_UpKeyCode))
-            l_Movement += l_ForwardDirection;
-        else if (Input.GetKey(m_DownKeyCode))
-            l_Movement += -l_ForwardDirection;
-
-        float l_SpeedMultiplier = 1.0f;
-        if (Input.GetKey(m_RunKeyCode))
-            l_SpeedMultiplier = m_SpeedMultiplier;
-
-        l_Movement.Normalize();
-        l_Movement *= m_Speed * l_SpeedMultiplier * Time.deltaTime;
-
-        m_VerticalSpeed = m_VerticalSpeed + Physics.gravity.y * m_GravityMultiplier * Time.deltaTime;
-        l_Movement.y = m_VerticalSpeed * Time.deltaTime;
-
-        CollisionFlags l_CollisionFlags = m_CharacterController.Move(l_Movement);
-
-        if (((m_VerticalSpeed < 0.0f) && ((l_CollisionFlags & CollisionFlags.Below) != 0)))
+        else if ((l_Flags & CollisionFlags.Above) != 0)
         {
-            m_VerticalSpeed = 0.0f;
-        }
-        else if (m_VerticalSpeed > 0.0f && (l_CollisionFlags & CollisionFlags.Above) != 0)
-        {
-            m_VerticalSpeed = 0.0f;
-        }
-
-        if (Input.GetKeyDown(m_JumpKeyCode))
-        {
-            m_VerticalSpeed = m_JumpSpeed;
+            m_VerticalSpeed = 0;
         }
     }
 }
